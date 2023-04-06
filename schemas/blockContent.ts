ef3b667a7dd83cf14d92fe3d6c8620b437192864
superiorkid <bayiPetani@protonmail.com>
2023-04-06 13:01:44 +0800

    improve post editor

--- schemas/blockContent.ts
+++ schemas/blockContent.ts
@@ -4,13 +4,34 @@ const blockContent = {
   type: 'array',
   of: [
     {
-      title: 'Block',
       type: 'block',
+      styles: [
+        {title: 'Normal', value: 'normal'},
+        {title: 'H1', value: 'h1'},
+        {title: 'H2', value: 'h2'},
+        {title: 'H3', value: 'h3'},
+        {title: 'H4', value: 'h4'},
+        {title: 'H5', value: 'h5'},
+        {title: 'H6', value: 'h6'},
+        {title: 'Quote', value: 'blockquote'}
+      ],
+      lists: [
+        {title: 'Bullet', value: 'bullet'},
+        {title: 'Numbered', value: 'numbered'}
+      ],
+      marks; [
+        {title: 'Strong', value: 'string'},
+        {title: 'Emphasis', value: 'em'},
+        {title: 'Code', value: 'code'}
+      ]
     },
     {
       type: 'image',
       options: {hotspot: true},
     },
+    {
+      type: 'code',
+    }
   ],
 }
 
